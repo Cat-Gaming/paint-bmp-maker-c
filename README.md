9a8d82a2925e69725c895e5e25f9589145ec3bbd
Tyler Wendorff <wendorfftyler@gmail.com>
2023-02-19 17:51:20 -0800

    License added.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Description
+This Program has 2 programs in it `main()` and `othermain()`
+I created a bmp image writer which writes down bitmap data
+using a pointer.
+The othermain program does stuff like writing down pixel info
+to stdout and using user input i do stuff.
+idk i just wrote this for fun!
+
+# Building
+This program requires no dependencies other than the standard C library. You can use any compiler for this.
+
+# License
+[title](LICENSE.md)
--- test.c
+++ test.c
@@ -1,11 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-// Author:
-// Tyler Wendorff
-// Copyright 2023
-// All Rights Reserved.
-//
+// License:
+/* Copyright (C) 2023  Tyler Wendorff
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 // Description:
 // This Program has 2 programs in it main() and othermain()
 // I created a bmp image writer which writes down bitmap data
